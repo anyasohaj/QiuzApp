4c9546b13b69de818c2259a05c1de182fb339658
Girasol <anyasohaj@gmail.com>
2018-01-07 06:33:33 +0200

    Became multi screen

--- app/src/main/java/com/bagheerapp/qiuzapp/MainActivity.java
+++ app/src/main/java/com/bagheerapp/qiuzapp/MainActivity.java
@@ -1,170 +1,38 @@
 package com.bagheerapp.qiuzapp;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Color;
+import android.icu.text.DecimalFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    int points = 0;
-    int counter = 0;
+  //  DecimalFormat df = new DecimalFormat("#.00");
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         }
 
-    public void checking (View view) {
-        CheckBox answerCheck1 = (CheckBox) findViewById(R.id.checkBox1);
-        answerCheck1.setTextColor(Color.parseColor("#388E3C"));
-        if (answerCheck1.isChecked()) {
-            points += 1;
-        }
-
-        CheckBox checkAnswerWrong1 = (CheckBox) findViewById(R.id.checkBox2);
-        checkAnswerWrong1.setTextColor(Color.RED);
-        if (checkAnswerWrong1.isChecked()) {
-            checkAnswerWrong1.setChecked(false);
-            answerCheck1.setChecked(true);
-        }
-
-        CheckBox checkAnswerWrong2 = (CheckBox) findViewById(R.id.checkBox3);
-        checkAnswerWrong2.setTextColor(Color.RED);
-        CheckBox checkAnswerWrong3 = (CheckBox) findViewById(R.id.checkBox4);
-        checkAnswerWrong3.setTextColor(Color.RED);
-    }
-
-    public void askNewQuestion (View view){
-        counter += 1;
-
-        TextView questionTextView = (TextView) findViewById(R.id.questionTextView);
-        questionTextView.setText(questionsAnswers[counter][0]);
-
-        CheckBox answerCheck1 = (CheckBox) findViewById(R.id.checkBox1);
-        answerCheck1.setText(Answersorder()[0]);
-        if (answerCheck1.isChecked()) {
-            answerCheck1.setChecked(false);
-        }
-
-        CheckBox answerCheck2 = (CheckBox) findViewById(R.id.checkBox2);
-        answerCheck2.setText(Answersorder()[1]);
-        if (answerCheck2.isChecked()) {
-            answerCheck2.setChecked(false);
-        }
-
-        CheckBox answerCheck3 = (CheckBox) findViewById(R.id.checkBox3);
-        answerCheck3.setText(Answersorder()[2]);
-        if (answerCheck3.isChecked()) {
-            answerCheck3.setChecked(false);
-        }
-
-        CheckBox answerCheck4 = (CheckBox) findViewById(R.id.checkBox4);
-        answerCheck4.setText(Answersorder()[3]);
-        if (answerCheck4.isChecked()) {
-            answerCheck4.setChecked(false);
-        }
-        answerCheck1.setTextColor(Color.BLACK);
-        answerCheck2.setTextColor(Color.BLACK);
-        answerCheck3.setTextColor(Color.BLACK);
-        answerCheck4.setTextColor(Color.BLACK);
-
-    }
-
-
-         public static String getRandomAnswers(String[] [] array) {
-             int rnd = new Random().nextInt(array.length);
-             if (rnd != 0) {
-                 return array[rnd][1];
-             } else {
-                      return array[29][1];
-                    }
-         }
-
-
-
-
-    public String[] Answersorder(){
-        String order[] = new String[4];
-        for (int i=0; i<2; i++) {
-            order[i] = getRandomAnswers(questionsAnswers);
-            }
-         order[3] = questionsAnswers[counter][1];
-
-        // permutation
-
-        int randomNumber = new Random().nextInt(3)+1;
-        String temp = order[randomNumber];
-        order[randomNumber] = order [3];
-        order[3] = temp;
-
-        return order;
-        }
- /*
-    private void randomOtherAnswers() {
-        Random rand = new Random();
-        int n = rand.nextInt(4) + 1;
-          int[] indexes = new int[] {1,2,3,4};
-          for (int i = 0; i<3; i++) {
-              int temp = indexes [i];
-              indexes[i] = indexes[n];
-              indexes[n] = temp;
-          }
-    }*/
-
-
-    // Tömbök deklarálása
-    String [] []  questionsAnswers  = new String[] [] {
-            {"Konstans fordulatszám", "G97 S_"},
-            {"Abszolút méretmegadás", "G90"},
-            {"Körinterpoláció óra járással ellentétesen az előtolás irányából nézve", "G3"},
-            {"X-Y sík", "G17"},
-            {"Metrikus méretmegadás", "G21"},
-            {"Főorsó óra járással ellentétesen forog a főorsóból nézve", "M4"},
-            {"Konstans vágósebesség","G96 S_"},
-            {"Szerszámhívás","T_ _ _ _"},
-            {"Lekerekítés",",R_"},
-            {"Kontúrkövetés balról az előtolás irányából","G41"},
-            {"Kontúrkövetés kikapcsolva / szerszámsugár korrekció kikapcsolva","G40"},
-            {"Z-X sík","G18"},
-            {"Első munkadarab nullpont","G54"},
-            {"Percenkénti előtolás","G94 F_"},
-            {"Egyenes interpoláció gyorsmenetben","G0"},
-            {"Kontúrkövetés jobbról az előtolás irányából","G42"},
-            {"Főorsó áll","M5"},
-            {"X tengely körüli forgó tengely","A"},
-            {"Z tengely körüli forgó tengely","C"},
-            {"Hűtő kenő folyadék bekapcsolva","M8"},
-            {"Főorsó óra mutató járással megegyezően forog a főorsóból nézve","M3"},
-            {"Maximális főorsó fordulat programozása","G92 S_"},
-            {"Szerszámállás kódja","Q _"},
-            {"Letörés",",C_"},
-            {"Szög programozása",",A_"},
-            {"Fordulatonkénti előtolás","G95 F_"},
-            {"Inches méretmegadás","G20"},
-            {"Program vége","M30"},
-            {"Egyenes interpoláció programozott előtolással","G1"},
-            {"Gépi koordináta rendszer","G53"}
-    };
-
+   public void startTest(View view){
+       Intent intent = new Intent(this, Test.class);
+       startActivity(intent);
+   }
 
 }
 
-//** question: string[sorszam: 1..30]
-//
-// public static int[] RandomizeArray(int[] array){
-//Random rgen = new Random();  // Random number generator
 
-//		for (int i=0; i<array.length; i++) {
- //   int randomPosition = rgen.nextInt(array.length);
-  //      int temp = array[i];
- //       array[i] = array[randomPosition];
- //       array[randomPosition] = temp;
-//        }
-
-//        return array;
-//        }*/
---
+++ app/src/main/java/com/bagheerapp/qiuzapp/Test.java
@@ -0,0 +1,215 @@
+package com.bagheerapp.qiuzapp;
+
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+public class Test extends AppCompatActivity {
+    int point = 0;
+    int points = 0;
+    int counter = 0;
+    String globalOrder[] = new String[4];
+    String questionsAnswers[][] = new String[30][2];
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+        gettingResou();
+    }
+
+    public void checking (View view) {
+        CheckBox answerCheck1 = (CheckBox) findViewById(R.id.checkBox1);
+        CheckBox answerCheck2 = (CheckBox) findViewById(R.id.checkBox2);
+        CheckBox answerCheck3 = (CheckBox) findViewById(R.id.checkBox3);
+        CheckBox answerCheck4 = (CheckBox) findViewById(R.id.checkBox4);
+
+
+        // Evaluation of the first (XML) question
+
+        if (counter == 0) {
+            if (answerCheck1.isChecked()) {points += 1;} else{
+                String wrong = getResources().getString(R.string.wrong_answer);
+                Toast.makeText(this, wrong, Toast.LENGTH_SHORT).show();
+                answerCheck1.setTextColor(Color.parseColor("#2E7D32")); answerCheck1.setChecked(true);
+                answerCheck2.setTextColor(Color.RED); answerCheck2.setChecked(false);
+                answerCheck3.setTextColor(Color.RED); answerCheck3.setChecked(false);
+                answerCheck4.setTextColor(Color.RED); answerCheck4.setChecked(false);
+                return;
+            }
+
+
+
+
+        } else {
+
+            if (answerCheck1.getText() != questionsAnswers[counter][1]) {
+                answerCheck1.setTextColor(Color.RED);
+
+                if (answerCheck1.isChecked()) {
+                    String wrong = getResources().getString(R.string.wrong_answer);
+                    Toast.makeText(this, wrong, Toast.LENGTH_SHORT).show();
+                    answerCheck1.setChecked(false);
+                }
+            } else {if (answerCheck1.isChecked()) {
+                point = 1;}
+                answerCheck1.setTextColor(Color.parseColor("#2E7D32"));
+                answerCheck1.setChecked(true);
+
+            }
+
+            if (answerCheck2.getText() != questionsAnswers[counter][1]) {
+                answerCheck2.setTextColor(Color.RED);
+
+                if (answerCheck2.isChecked()) {
+                    String wrong = getResources().getString(R.string.wrong_answer);
+                    Toast.makeText(this, wrong, Toast.LENGTH_SHORT).show();
+                    answerCheck2.setChecked(false);
+                }
+            } else {if (answerCheck2.isChecked()) {
+                point = 1;}
+                answerCheck2.setTextColor(Color.parseColor("#2E7D32"));
+                answerCheck2.setChecked(true);
+
+            }
+
+            if (answerCheck3.getText() != questionsAnswers[counter][1]) {
+                answerCheck3.setTextColor(Color.RED);
+
+                if (answerCheck3.isChecked()) {
+                    String wrong = getResources().getString(R.string.wrong_answer);
+                    Toast.makeText(this, wrong, Toast.LENGTH_SHORT).show();
+                    answerCheck3.setChecked(false);
+                }
+            } else {if (answerCheck3.isChecked()) {
+                point = 1;}
+                answerCheck3.setTextColor(Color.parseColor("#2E7D32"));
+                answerCheck3.setChecked(true);
+
+            }
+
+            if (answerCheck4.getText() != questionsAnswers[counter][1]) {
+                answerCheck4.setTextColor(Color.RED);
+
+                if (answerCheck4.isChecked()) {
+                    String wrong = getResources().getString(R.string.wrong_answer);
+                    Toast.makeText(this, wrong, Toast.LENGTH_SHORT).show();
+                    answerCheck4.setChecked(false);
+                }
+            } else {if (answerCheck4.isChecked()) {
+                point = 1;}
+                answerCheck4.setTextColor(Color.parseColor("#2E7D32"));
+                answerCheck4.setChecked(true);
+
+            }
+
+        }
+
+    }
+
+
+    // Clicking the NEXT button it generate new question, new answer possibilities and reset checkboxes
+
+    public void askNewQuestion (View view) {
+        if (point == 1) {
+            points += 1;
+            point = 0;
+        }
+
+        if (counter > 28) {
+            String endToast = getResources().getString(R.string.End_toast);
+            Toast.makeText(this, endToast, Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        counter += 1;
+        TextView countUp = (TextView) findViewById(R.id.textView);
+        countUp.setText((counter + 1) + "/30");
+
+
+        if (counter == 29) {
+            double percent = points / 0.3;
+            int intValue = (int) percent;
+            TextView evaluation = (TextView) findViewById(R.id.textView);
+            String acc = getResources().getString(R.string.accomplish);
+            evaluation.setText(acc + " " + intValue + "%");
+        }
+
+
+        generateRandomNumbers();
+
+        TextView questionTextView = (TextView) findViewById(R.id.questionTextView);
+        questionTextView.setText(questionsAnswers[counter][0]);
+
+        CheckBox answerCheck1 = (CheckBox) findViewById(R.id.checkBox1);
+        answerCheck1.setText(globalOrder[0]);
+        if (answerCheck1.isChecked()) {
+            answerCheck1.setChecked(false);
+        }
+
+        CheckBox answerCheck2 = (CheckBox) findViewById(R.id.checkBox2);
+        answerCheck2.setText(globalOrder[1]);
+        if (answerCheck2.isChecked()) {
+            answerCheck2.setChecked(false);
+        }
+
+        CheckBox answerCheck3 = (CheckBox) findViewById(R.id.checkBox3);
+        answerCheck3.setText(globalOrder[2]);
+        if (answerCheck3.isChecked()) {
+            answerCheck3.setChecked(false);
+        }
+
+        CheckBox answerCheck4 = (CheckBox) findViewById(R.id.checkBox4);
+        answerCheck4.setText(globalOrder[3]);
+        if (answerCheck4.isChecked()) {
+            answerCheck4.setChecked(false);
+        }
+        answerCheck1.setTextColor(Color.BLACK);
+        answerCheck2.setTextColor(Color.BLACK);
+        answerCheck3.setTextColor(Color.BLACK);
+        answerCheck4.setTextColor(Color.BLACK);
+
+    }
+
+
+
+    public  void generateRandomNumbers() {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < 30; i++) {
+            list.add(new Integer(i));
+        }
+        Collections.shuffle(list);
+        for (int j = 0; j < 3; j++) {
+            globalOrder [j] = questionsAnswers[list.get(j)][1];
+            if (globalOrder[j] == questionsAnswers[counter][1]) {
+                globalOrder[j] = "G100";
+            }
+            globalOrder[3] = questionsAnswers[counter][1];
+        }
+        // Permutation
+
+        int randomNumber = new Random().nextInt(4);
+        String temp = globalOrder[randomNumber];
+        globalOrder[randomNumber] = globalOrder [3];
+        globalOrder[3] = temp;
+    }
+
+    public void gettingResou () {
+        Resources res = getResources();
+        String[] togetherFromResou = res.getStringArray(R.array.questions_array);
+
+        for (int k = 0; k < 30; k++) {
+            questionsAnswers[k] = togetherFromResou[k].split("/");
+
+        } }
+}
